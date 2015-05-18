4892e2c9bedcdaca86a3e2a3f8bb27fa909c506c
Doug Wright <mrdougwright@gmail.com>
2015-05-17 23:14:51 -0700

    remove unnecessary gmap stuff

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -3,96 +3,32 @@ $(window).load(function() {
 });
 
 var map;
-var poly;
+
 function initialize() {
-        var markerCoords1 = new google.maps.LatLng(30.055487, 31.279766);
-        var markerCoords2 = new google.maps.LatLng(30.543523, 31.656232);
-        var mapOptions = {
-          center: new google.maps.LatLng(30.055487, 31.279766), // Oakland 37.7919615,-122.2287941
-          zoom: 8, //12
-          mapTypeId: google.maps.MapTypeId.NORMAL
-        };
-        // initializing map
-        map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-        
-        // simple marker
-        var marker = createMarker(markerCoords1, map, "Hi");
-        
-        // custom marker
-        var customMarker = createCustomMarker(markerCoords2, map, "Hi");
+  var markerCoords1 = new google.maps.LatLng(37.7919615,-122.2287941);
+  var markerCoords2 = new google.maps.LatLng(38.2919615,-122.7287941);
+  var mapOptions = {
+    center: new google.maps.LatLng(37.7919615,-122.2287941), // Oakland
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.NORMAL
+  };
+  // initializing map
+  map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
 
-        // add infowindow when clicking on the simple marker marker
-        var info = createInfoWindow("Congratulation!");
-        google.maps.event.addListener(marker, 'click', function() {
-          info.open(map,marker);
-        });
+  // simple marker
+  var marker = createMarker(markerCoords1, map, "Hi");
 
-        // drawing points
-        /*google.maps.event.addListener(map, 'click', function(e){
-          createMarker(getCurrentPosition(e), map, "Hello World!");
-        });*/
+  // custom marker
+  var customMarker = createCustomMarker(markerCoords2, map, "Hi");
 
-
-        // drawing dynamic polyline
-          var polyOptions = {
-            strokeColor: '#000000',
-            strokeOpacity: 1.0,
-            strokeWeight: 3
-          };
-        poly = new google.maps.Polyline(polyOptions);
-        poly.setMap(map);
-        google.maps.event.addListener(map, 'click', addLatLng);
-        
-        // drawing custom dynamic polyline ( with dashes/ symbols)
-        // Define the symbol, using one of the predefined paths ('CIRCLE')
-        // supplied by the Google Maps JavaScript API.
-        var lineSymbol = {
-          path: google.maps.SymbolPath.CIRCLE,
-          scale: 8,
-          strokeColor: '#393'
-        };
-        // drawing static polyline
-        var lineCoordinates = [
-          new google.maps.LatLng(30.055487, 31.279766),
-          new google.maps.LatLng(30.223356, 31.324345),
-          new google.maps.LatLng(30.345656, 31.567677),
-          new google.maps.LatLng(30.565678, 31.676887)
-        ];
-        createPolyline(map,lineCoordinates,lineSymbol);
-        animateCircle();
-
-   }
-function animateCircle() {
-    var count = 0;
-    window.setInterval(function() {
-      count = (count + 1) % 200;
-
-      var icons = line.get('icons');
-      icons[0].offset = (count / 2) + '%';
-      line.set('icons', icons);
-  }, 20);
-}
-
-function addLatLng(event){  
-    var path = poly.getPath();
-    // Because path is an MVCArray, we can simply append a new coordinate
-    // and it will automatically appear.
-    path.push(event.latLng);
-}
-function createPolyline(map,lineCoordinates,lineSymbol){
-   var linePath = new google.maps.Polyline({
-    path: lineCoordinates,
-    geodesic: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-     icons: [{
-      icon: lineSymbol,
-      offset: '100%'
-    }]
+  // add infowindow when clicking on the simple marker marker
+  var info = createInfoWindow("Mtg title");
+  google.maps.event.addListener(marker, 'click', function() {
+    info.open(map,marker);
   });
-   linePath.setMap(map);
-}
+} // <-- end of initializer
+
+
 
 function createInfoWindow(text){
   var infowindow = new google.maps.InfoWindow({
@@ -112,7 +48,7 @@ function createMarker(coords, map, title){
     position: coords,
     map: map,
     title: title,
-    draggable: true,
+    draggable: false,
     animation: google.maps.Animation.DROP
   });
     return marker;
@@ -123,7 +59,7 @@ function createCustomMarker(coords,map,title){
     map: map,
     title: title,
     icon: createImage("/assets/icon.png"),
-    draggable: true,
+    draggable: false,
     animation: google.maps.Animation.DROP
   }); 
     return marker;
