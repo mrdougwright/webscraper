626af75db6a18091c640c7fa250f3dc1d5d40643
Doug Wright <mrdougwright@gmail.com>
2015-05-19 00:53:25 -0700

    clean up js; css; still fleshing out legend

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,79 +1,60 @@
 $(window).load(function() {
-  loadScript();
-});
+  loadScript()
+})
 
 var map, marker;
+var $legend = $('#legend')
 
 function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(37.7919615,-122.2287941), // Oakland
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.NORMAL
-  };
+  }
   // initializing map
-  map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-
-  // places.forEach(showMapPoints)  >> Use Geocoder gem
-  showMapPoints(places[0]) // temporary until geocoded....
-} // <-- end of initializer
-
-function createInfoWindow(text){
-  var infowindow = new google.maps.InfoWindow({
-    content: text
-  });
-  return infowindow;
+  map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions)
+  places.forEach(mapPoints) // window.places from db
 }
 
-function getCurrentPosition(e){
-  var markercoords = new google.maps.LatLng(e.latLng.lat(),e.latLng.lng());
-  return markercoords;
+function mapPoints(loc_data, index, array) {
+  createMarker(loc_data, map)
 }
 
-function createMarker(coords, map, title){
+function renderLegend(loc_data) {
+  $legend.find('.name').text(loc_data.name)
+  $legend.find('.address').text(loc_data.address)
+  $legend.find('.city').text(loc_data.city)
+  $legend.find('.time').text(loc_data.time)
+  debugger
+}
+
+function createMarker(data, map){
+  var coordinates = new google.maps.LatLng(data.latitude, data.longitude)
   marker = new google.maps.Marker({
-    position: coords,
+    position: coordinates,
     map: map,
-    title: title,
+    title: data.name,
     draggable: false,
     animation: google.maps.Animation.DROP
-  });
+  })
   
-  // add infowindow when clicking on the simple marker marker
   google.maps.event.addListener(marker, 'click', function() {
-    createInfoWindow(title).open(map,marker);
-  });
-
-  return marker;
+    bubbleWindow(this.title).open(map,this)
+    return renderLegend(data)
+  })
+  return marker
 }
 
-function codeAddress(address){
-  var geocoding = new google.maps.Geocoder();
-
-  if(address.length > 0){
-    geocoding.geocode({'address': address},function(results, status){
-      if(status == google.maps.GeocoderStatus.OK){
-        createMarker(results[0].geometry.location, map, "testing")
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
-    });
-  }
-}
-
-function showMapPoints(element, index, array){
-  codeAddress(element.address) // use geocoder gem instead
+function bubbleWindow(text){
+  var infowindow = new google.maps.InfoWindow({ content: text })
+  return infowindow
 }
 
 
 function loadScript() {
-	console.log("map loading ...");
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  //'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBJYFdplGeKUUEmGZ-vL4ydiSZ09Khsa_o&sensor=false&libraries=drawing'
-  script.src = 'https://maps.googleapis.com/maps/api/js?sensor=false' +
-    //'&v=3.14'+
-    //'&key=AIzaSyBJYFdplGeKUUEmGZ-vL4ydiSZ09Khsa_o'+
-    //'&libraries=drawing'+
-    '&callback=initialize';
-  document.body.appendChild(script);
+	console.log("map loading ...")
+  var script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize'
+  document.body.appendChild(script)
 }
